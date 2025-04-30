cebdfef67a87e38b7332608680fc429864903000
Ilya Yudin <yudin2402@gmail.com>
2025-04-30 19:42:47 +0200

    added header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const selectors = {
-    NAV: '.js-nav',
+    HEADER: '.js-header',
     BURGER: '.js-burger',
     BODY: 'body',
   };
@@ -9,24 +9,22 @@ document.addEventListener('DOMContentLoaded', () => {
     NO_SCROLL: 'no-scroll'
   }
 
-  const nav = document.querySelector(selectors.NAV);
+  const header = document.querySelector(selectors.HEADER);
   const burger = document.querySelector(selectors.BURGER);
   const body = document.querySelector(selectors.BODY);
 
-  if(!nav || !burger) {
+  if(!header || !burger) {
     return;
   }
 
   burger.addEventListener('click', toggleNav);
 
   function toggleNav() {
-    if(nav.classList.contains(classes.ACTIVE)) {
-      nav.classList.remove(classes.ACTIVE);
-      burger.classList.remove(classes.ACTIVE);
+    if(header.classList.contains(classes.ACTIVE)) {
+      header.classList.remove(classes.ACTIVE);
       body.classList.remove(classes.NO_SCROLL);
     } else {
-      nav.classList.add(classes.ACTIVE);
-      burger.classList.add(classes.ACTIVE);
+      header.classList.add(classes.ACTIVE);
       body.classList.add(classes.NO_SCROLL);
     }
   }
