0064e8a164be6c780ab2b052b76b1c7dd1b4f753
Ilya Yudin <yudin2402@gmail.com>
2025-05-04 12:53:12 +0200

    added extensions section

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     HEADER: '.js-header',
     BURGER: '.js-burger',
     BODY: 'body',
+    TABS: '.js-tabs',
+    TABS_BTN: '.js-tabs-btn',
+    TABS_SECTION: '.js-tabs-section'
   };
   const classes = {
     ACTIVE: 'active',
@@ -13,9 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const burger = document.querySelector(selectors.BURGER);
   const body = document.querySelector(selectors.BODY);
 
-  if(!header || !burger) {
-    return;
-  }
+  if(!header || !burger) return;
 
   burger.addEventListener('click', toggleNav);
 
@@ -28,4 +29,30 @@ document.addEventListener('DOMContentLoaded', () => {
       body.classList.add(classes.NO_SCROLL);
     }
   }
+
+  const tabs = document.querySelector(selectors.TABS);
+
+  if(!tabs) return;
+
+  const tabsBtns = tabs.querySelectorAll(selectors.TABS_BTN);
+  const tabsSections = tabs.querySelectorAll(selectors.TABS_SECTION);
+
+  function setActiveTab(index) {
+    tabsBtns.forEach((btn) => btn.classList.remove(classes.ACTIVE));
+    tabsSections.forEach((section) => section.classList.remove(classes.ACTIVE));
+  
+    tabsBtns[index].classList.add(classes.ACTIVE);
+    tabsSections[index].classList.add(classes.ACTIVE);
+  }
+
+  tabs.addEventListener('click', (event) => {
+    const clickedBtn = event.target.closest(selectors.TABS_BTN);
+    if (!clickedBtn) return;
+  
+    const index = Array.from(tabsBtns).indexOf(clickedBtn);
+
+    if (index !== -1) {
+      setActiveTab(index);
+    }
+  });
 });
